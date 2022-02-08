2e25f6306f9e11ec655ccd96dbb06a75b937e1b2
HyeongjaeLee <tlwprosper@gmail.com>
2022-02-09 00:47:49 +0900

    fix: 샘플영상 링크 변경

--- src/components/common/VideoPlayer.js
+++ src/components/common/VideoPlayer.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
+import React from "react";
+import ReactPlayer from "react-player";
 
 const VideoPlayer = () => {
-    return (
-        <ReactPlayer className='video-player-wrap' url='https://www.youtube.com/watch?v=7C2z4GqqS5E' playing controls />
-    );
+  return (
+    <ReactPlayer
+      className="video-player-wrap"
+      url="https://www.youtube.com/watch?v=38hvLwK_BH4"
+      playing
+      controls
+    />
+  );
 };
 
 export default VideoPlayer;
