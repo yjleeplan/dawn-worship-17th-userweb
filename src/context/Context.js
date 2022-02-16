ed7674b88be66a9831e9004e9f4bc0ec5180e0c1
Yangjae Lee <yjlee@Yangjaeui-Macmini.local>
2022-02-16 09:55:51 +0900

    fix: 보너스 스티커 시작시간 잘못된 문제

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,7 +1,7 @@
 export const ATTENDANCE_START_HOUR = 4;
 export const ATTENDANCE_END_HOUR = 8;
 
-export const BONUS_START_HOUR = 8;
+export const BONUS_START_HOUR = 20;
 export const BONUS_END_HOUR = 24;
 
 export const BONUS_DAYS = [16, 23];
