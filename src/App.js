ef7d897c67d3bd8fb0da97e6f5d1bc12305b4e1d
HyeongjaeLee <tlwprosper@gmail.com>
2022-02-21 20:55:06 +0900

    fix: BrowserRouter, ConfigProvider 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import './assets/css/index.css';
-import React from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import Routes from './routes/Routes';
-import { store, persistor } from './stores';
+import { ConfigProvider } from "antd";
+import koKR from "antd/lib/locale/ko_KR";
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import "./assets/css/index.css";
+import Routes from "./routes/Routes";
+import { persistor, store } from "./stores";
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Routes />
-      </PersistGate>
-    </Provider>
+    <BrowserRouter>
+      <ConfigProvider locale={koKR}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Routes />
+          </PersistGate>
+        </Provider>
+      </ConfigProvider>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
